5877650cf746c4065833543bb1d686569b7defea
samuel0122 <samuoliva9@gmail.com>
2024-09-16 21:49:43 +0200

    Añadido DS_Store al gitignore

--- main.py
+++ main.py
@@ -107,5 +107,5 @@ if __name__ == '__main__':
         port = int(os.environ.get("PORT", 5000))
         app.run(debug=True, host='0.0.0.0', port=port)
     except:
-        logger.critical(f"Error crítico al iniciar la aplicación Flask: {str(e)}")
+        logger.critical(f"Error crítico al iniciar la aplicación Flask")
         raise  # Re-lanzar la excepción para que también se registre en la consola
